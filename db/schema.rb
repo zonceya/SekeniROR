70cb2f8b24a0ebd343a44a921de70eb485610037
Zama Onceya <zonceya@gmail.com>
2025-02-18 21:57:58 +0200

    Add existing project files to Git

---
+++ db/schema.rb
@@ -0,0 +1,51 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 0) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "profile", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "profile_picture", limit: 255
+    t.string "mobile", limit: 255
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+    t.datetime "updated_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  end
+
+  create_table "user_sessions", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token", limit: 255, null: false
+    t.jsonb "meta"
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+    t.datetime "ended_at", precision: nil
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.string "email", limit: 255, null: false
+    t.string "mobile", limit: 15
+    t.string "username", limit: 255
+    t.string "auth_mode", limit: 50, null: false
+    t.boolean "status", default: true
+    t.boolean "deleted", default: false
+    t.string "profile_picture", limit: 255
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+    t.datetime "updated_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+
+    t.unique_constraint ["email"], name: "users_email_key"
+    t.unique_constraint ["username"], name: "users_username_key"
+  end
+
+  add_foreign_key "profile", "users", name: "profile_user_id_fkey"
+  add_foreign_key "user_sessions", "users", name: "user_sessions_user_id_fkey"
+end
