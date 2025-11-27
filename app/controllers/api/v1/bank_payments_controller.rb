d7be7d73795129a766c35a8e01901a91e5455464
GROSAPACKS\zonce <zonceya@gmail.com>
2025-11-27 16:24:14 +0200

    "Fix syntax error in BankPaymentsController by recreating file

--- app/controllers/api/v1/bank_payments_controller.rb
+++ app/controllers/api/v1/bank_payments_controller.rb
@@ -1,42 +1,43 @@
-# app/controllers/bank_payments_controller.rb
 module Api
   module V1
-   class BankPaymentsController < ApplicationController
-  before_action :authenticate_user!
+    class BankPaymentsController < ApplicationController
+      before_action :authenticate_user!
 
-  def create
-    @item = Item.find(params[:item_id])
-    
-    result = Inventory::HoldService.new(
-      item: @item,
-      user: current_user,
-      quantity: params[:quantity] || 1
-    ).call
+      def create
+        @item = Item.find(params[:item_id])
 
-    if result.success?
-      order = create_pending_order(result.hold)
-      redirect_to bank_transfer_instructions_path(order)
-    else
-      flash[:error] = result.error
-      redirect_to item_path(@item)
-    end
-  end
+        result = Inventory::HoldService.new(
+          item: @item,
+          user: current_user,
+          quantity: params[:quantity] || 1
+        ).call
 
-  private
+        if result.success?
+          order = create_pending_order(result.hold)
+          redirect_to bank_transfer_instructions_path(order)
+        else
+          flash[:error] = result.error
+          redirect_to item_path(@item)
+        end
+      end
 
-  def create_pending_order(hold)
-    Order.create!(
-      buyer: current_user,
-      shop: hold.item.shop,
-      order_status: :pending_payment,
-      payment_status: :unpaid,
-      order_items_attributes: [{
-        item: hold.item,
-        quantity: hold.quantity,
-        actual_price: hold.item.price
-      }]
-    ).tap do |order|
-      hold.update!(order: order)
+      private
+
+      def create_pending_order(hold)
+        Order.create!(
+          buyer: current_user,
+          shop: hold.item.shop,
+          order_status: :pending_payment,
+          payment_status: :unpaid,
+          order_items_attributes: [{
+            item: hold.item,
+            quantity: hold.quantity,
+            actual_price: hold.item.price
+          }]
+        ).tap do |order|
+          hold.update!(order: order)
+        end
+      end
     end
   end
 end
