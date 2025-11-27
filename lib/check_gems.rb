9364734aeb052556ea9b206bfdb7683d822a118b
GROSAPACKS\zonce <zonceya@gmail.com>
2025-11-27 03:05:26 +0200

    lib/check_gems.rb rm

--- lib/check_gems.rb
+++
@@ -1,4 +0,0 @@
-# lib/check_gems.rb
-puts "Googleauth version: #{Gem.loaded_specs['googleauth']&.version}"
-puts "Available methods on ServiceAccountCredentials:"
-puts Google::Auth::ServiceAccountCredentials.methods.sort
